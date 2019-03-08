4e7f592c4bed0c265de985e3305d86c73406a017
saul365 <saul_bh365@comunidad.unam.mx>
2019-03-08 08:43:07 -0600

    working dinamic programming

--- Factorial.java
+++ Factorial.java
@@ -6,8 +6,14 @@ public class Factorial{
 	public static void main(String[] args){ 
 		System.out.println("Este programa calcula el factorial de 1,000,000 con programacion dinamica");
 		LinkedList<BigInteger> pd=new LinkedList<BigInteger>();
-		BigInteger res = fact(6/*1000000*/,pd);
+		long it= System.currentTimeMillis();
+		BigInteger res = fact(/*6*/1000000,pd);
 		System.out.println("El resultado es: "+res.toString());
+		System.out.println("En un tiempo de: "+(System.currentTimeMillis()-it)/1000);
+		it= System.currentTimeMillis();
+		res = fact(/*6*/1000000,pd);
+		System.out.println("El resultado es: "+res.toString());
+		System.out.println("En un tiempo de: "+(System.currentTimeMillis()-it)/1000);
 	}
 	public static BigInteger fact(int finale,LinkedList<BigInteger>pd){
 		int tam= pd.size();
@@ -22,15 +28,19 @@ public class Factorial{
 		int inicio=1;
 		BigInteger curr=new BigInteger("1");
 		if(tam>(tmp-2)&&(tam>0)){
-			return pd.get(tmp);
+			return pd.get(tmp-1);
 		}else if(tam>1){
 			curr=pd.getLast();
 			inicio=tmp*10000;
 		}
 		BigInteger res=curr;
+		BigInteger tenT= new BigInteger("10000");
 		for(;inicio<finale;inicio++){
 			res=curr.multiply(res);
 			curr=curr.add(BigInteger.ONE);
+			if(curr.mod(tenT).equals(BigInteger.ZERO)){
+				pd.add(res);
+			}
 		}
 		return res;
 	}
