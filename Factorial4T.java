ba16d48e14921da7bad33f51456a91a69d5961a9
saul365 <saul_bh365@comunidad.unam.mx>
2019-03-13 18:49:52 -0600

    save

--- Factorial4T.java
+++ Factorial4T.java
@@ -5,6 +5,16 @@ public class Factorial4T{
    FactorialT t2;
    FactorialT t3;
    FactorialT t4;
+
+   public Factorial4T(int inicio,int finale){      
+      int abs=finale-inicio;
+		boolean mod0=(abs%4==0)?true:false;
+		int part=abs/4;
+		int ph=part;
+      if(mod0){
+         t1=new FactorialT(inicio,
+      }
+   }
 	public static void main(String args[]){ 
 
 	}
