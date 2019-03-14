bd84ac9141d4841d4dd1211aeec4abbdf4cc74ac
saul365 <saul_bh365@comunidad.unam.mx>
2019-03-13 23:27:34 -0600

    up to date

--- Factorial4T.java
+++ Factorial4T.java
@@ -7,23 +7,23 @@ public class Factorial4T{
    FactorialT t4;
 
    public Factorial4T(int inicio,int finale){      
-      int abs=finale-inicio;
+      int abs=finale-inicio+1;
 		boolean mod0=(abs%4==0)?true:false;
 		int part=abs/4;
 		int ph=part;
-		int add=1;
+		int add=0;
       if(mod0){
          part++;
-			add=2;
+			add=1;
       }
       t1=new FactorialT(inicio,part);
 		inicio+=ph+add;
 		part+=ph;
       t2=new FactorialT(inicio,part);
-		inicio+=ph+1;
+		inicio+=ph;
 		part+=ph;
       t3=new FactorialT(inicio,part);
-		inicio+=ph+1;
+		inicio+=ph;
 		part+=ph;
       t4=new FactorialT(inicio,part);
 		if(part==finale){
@@ -32,10 +32,11 @@ public class Factorial4T{
          System.out.println("Error en este pedo");
       }
    }
-	public startOp(){
+	public void startOp(){
 		  System.out.println("ignora esto");
    }
 	public static void main(String args[]){ 
+		Factorial4T try1=new Factorial4T(1,1000000);
 
 	}
 }
