21d7f920a6bf6d9fd01a6a3839387abb8e992631
saul365 <saul_bh365@comunidad.unam.mx>
2019-03-13 19:19:58 -0600

    first 4T rev

--- Factorial4T.java
+++ Factorial4T.java
@@ -11,9 +11,29 @@ public class Factorial4T{
 		boolean mod0=(abs%4==0)?true:false;
 		int part=abs/4;
 		int ph=part;
+		int add=1;
       if(mod0){
-         t1=new FactorialT(inicio,
+         part++;
+			add=2;
       }
+      t1=new FactorialT(inicio,part);
+		inicio+=ph+add;
+		part+=ph;
+      t2=new FactorialT(inicio,part);
+		inicio+=ph+1;
+		part+=ph;
+      t3=new FactorialT(inicio,part);
+		inicio+=ph+1;
+		part+=ph;
+      t4=new FactorialT(inicio,part);
+		if(part==finale){
+         System.out.println("Exito empresarial");
+      }else{
+         System.out.println("Error en este pedo");
+      }
+   }
+	public startOp(){
+		  System.out.println("ignora esto");
    }
 	public static void main(String args[]){ 
 
